56874f525f4c52737752785607e53319fa722a28
mo.javadian@gmail.com <mo.javadian@gmail.com>
2018-03-17 14:45:32 +0330

    solve_problem_day1

--- python practice/day1/solution.py
+++ python practice/day1/solution.py
@@ -2,11 +2,22 @@ i = 4
 d = 4.0
 s = 'HackerRank '
 
+##int_var = int(input("enter an integer: "))
+##float_var = float(input("enter a float number: "))
+##string_var = str(input("enter a string: "))
+##
+##print("i + integer you have entered = ", i+int_var)
+##print("d + float you have entered = ", d+float_var)
+##print("s concat with string you have entered = ", s+string_var)
+
+
 integer = int(input())
 double = float(input())
 string = input()
 print(i+integer)
 print(d+double)
 print(s+string)
+
+
 
 
