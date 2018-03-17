3a4366b43e4cb20f543cd811b89cfaf022608385
mo.javadian@gmail.com <mo.javadian@gmail.com>
2018-03-17 16:02:25 +0330

    solve problem day3

--- python practice/day3/solution.py
+++ python practice/day3/solution.py
@@ -1,6 +1,19 @@
-n=int(input())
-m=n % 2
-if ((m!=0) or (n>=6 and n<=20)):
+##n=int(input())
+##m=n % 2
+##if ((m!=0) or (n>=6 and n<=20)):
+##    print("Weird")
+##elif ((n>=2 and n<=5)or (n>20)):
+##    print("Not Weird")
+
+
+n = int(input())
+
+if (n % 2) == 0 :
+    if (n >= 2 and n <= 5) :
+        print("Not Weird")
+    elif (n >= 6 and n <= 20) :
+        print("Weird")
+    elif (n > 20) :
+        print("Not Weird")
+else :
     print("Weird")
-elif ((n>=2 and n<=5)or (n>20)):
-    print("Not Weird")
